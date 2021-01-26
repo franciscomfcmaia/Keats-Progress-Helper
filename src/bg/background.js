5005cdd263b36f690d33f6da10e5e333168e5049
Francisco Maia <franciscomaia@Franciscos-MacBook-Pro.local>
2021-01-26 13:04:52 +0000

    v1.2 +toolpit options +improved modules recognision

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,3 +1,16 @@
+//Send messages to current content script
+var content = {
+  selectAllModules : function(){
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendRequest(tab.id, {cmd : "selectAllModules", payload : null}, null);
+    });
+  },
+  deleteAllActivities : function(){
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendRequest(tab.id, {cmd : "deleteAllActivities", payload : null}, null);
+    });
+  }
+}
 //This returns the current URL
 function getCurrentTabUrl(callback) {
   try {
@@ -10,6 +23,64 @@ function getCurrentTabUrl(callback) {
     //alert(err)
     callback(null)
   }
+}
+//Arrays equal
+function arraysEqual(_arr1, _arr2) {
+    if (
+      !Array.isArray(_arr1)
+      || !Array.isArray(_arr2)
+      || _arr1.length !== _arr2.length
+      ) {
+        return false;
+      }
+
+    // .concat() is used so the original arrays are unaffected
+    const arr1 = _arr1.concat().sort();
+    const arr2 = _arr2.concat().sort();
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+         }
+    }
+
+    return true;
+}
+//Checks if object is in array
+function containsObject(obj, list, keys_to_check) {
+    var x;
+    var checked = [];
+    for (x in list) {
+      checked = []
+      for(y in keys_to_check){
+        key_to_check = keys_to_check[y]
+        if(obj.hasOwnProperty(key_to_check) && list[x].hasOwnProperty(key_to_check)){
+          if(obj[key_to_check] == list[x][key_to_check]){
+            checked.push(key_to_check)
+            if(arraysEqual(checked, keys_to_check)){
+              return true
+            }
+          }else{
+            //
+          }
+        }else{
+          //
+        }
+      }
+    }
+    return false;
+}
+
+//Create list without object
+function filter_array(obj, list){
+  var new_list = []
+  for(x in list){
+    current_obj = list[x]
+    if(!containsObject(obj, [current_obj], ["resourceURL"])){
+      new_list.push(current_obj)
+    }
+  }
+  return new_list
 }
 
 //This returns the module ID from the keats page
@@ -31,6 +102,7 @@ function getCurrentModuleID(callback){
 }
 
 var keatsProgressData = {
+  fifoBuffer : [],
   getProgressDataFromCurrentTab : function(callback){
     getCurrentModuleID(function(moduleID){
       if(moduleID!=null){
@@ -49,35 +121,59 @@ var keatsProgressData = {
       }
     });
   },
-  processCurrentModuleEntry : function(entry, callback){
+  directWrite : function(entries, callback){
+    this.setProgressDataFromCurrentTab(entries, function(result){
+      callback(true)
+    });
+  },
+  processCurrentModuleEntry : function(entry, callback, more_coming = false){
     //Get module stage from tab
-    this.getProgressDataFromCurrentTab((items) =>{
+    console.log(entry)
+    var writeData = (completed_activities_new) => {
+      if(more_coming){
+        for(activityIndex in completed_activities_new){
+          activity = completed_activities_new[activityIndex]
+          if(!containsObject(activity, this.fifoBuffer, ["resourceURL"])){
+            this.fifoBuffer.push(activity)
+            callback(true)
+          }
+        }
+      }else{
+        console.log(`Currently in buffer ${this.fifoBuffer.length}`)
+        if(this.fifoBuffer.length == 0){
+          this.setProgressDataFromCurrentTab(completed_activities_new, function(result){
+            callback(true)
+          });
+        }else{
+          console.log(`Flushing ${this.fifoBuffer.length} entries...`)
+          this.setProgressDataFromCurrentTab(this.fifoBuffer, function(result){
+            this.fifoBuffer = []
+            callback(true)
+          });
+        }
+      }
+    }
+    this.getProgressDataFromCurrentTab((items) => {
       if(!(Object.keys(items).length === 0 && items.constructor === Object)){
         var completed_activities = items[Object.keys(items)[0]];
-        if(completed_activities.includes(entry.activityID)){
+        if(containsObject(entry, completed_activities, ["resourceURL"])){
           if(entry.activityStatus){
             //do nothing
             callback(true)
           }else{
-            completed_activities_new = completed_activities.filter(e => e !== entry.activityID);
-            this.setProgressDataFromCurrentTab(completed_activities_new, function(result){
-              callback(true)
-            });
+            completed_activities_new = filter_array(entry, completed_activities)
+            writeData(completed_activities_new)
           }
         }else{
           if(entry.activityStatus){
-            completed_activities.push(entry.activityID)
-            this.setProgressDataFromCurrentTab(completed_activities, function(result){
-              callback(true)
-            });
+            completed_activities.push(entry)
+            writeData(completed_activities)
           }
         }
       }else{
         //Module has not been initialised yet.
         if(entry.activityStatus){
-          this.setProgressDataFromCurrentTab([entry.activityID], function(result){
-            callback(true)
-          });
+          writeData([entry])
         }
       }
     });
@@ -90,8 +186,23 @@ chrome.runtime.onMessage.addListener(
         sendResponse(progressData)
       });
     }
+    //if user is in main dashboard
+    if(request.cmd=="isNavigatedDashboard"){
+      getCurrentTabUrl(function(url){
+        if(url!=null){
+          sendResponse(url.endsWith("keats.kcl.ac.uk/my/"))
+        }else{
+          sendResponse(false)
+        }
+      });
+    }
     if(request.cmd=="processCurrentModuleEntry"){
-      keatsProgressData.processCurrentModuleEntry(request.payload, function(status){
+      keatsProgressData.processCurrentModuleEntry(request.payload.data, function(status){
+        sendResponse(status)
+      }, request.payload.more_coming);
+    }
+    if(request.cmd=="directWrite"){
+      keatsProgressData.directWrite(request.payload.data, function(status){
         sendResponse(status)
       });
     }
@@ -120,3 +231,32 @@ chrome.tabs.onUpdated.addListener(function
     }
   }
 );
+var clear_storage = function(){
+    chrome.storage.local.clear(function() {
+      var error = chrome.runtime.lastError;
+      if (error) {
+          console.error(error);
+      }
+  });
+}
+//contextMenus
+chrome.contextMenus.create({
+    id: "keats_progress",
+    title: "Keats Progress",
+    contexts: ["all"]
+});
+chrome.contextMenus.create({
+  title: "Mark All As Completed",
+  parentId: "keats_progress",
+  onclick: content.selectAllModules
+});
+chrome.contextMenus.create({
+  title: "Delete Page Progress",
+  parentId: "keats_progress",
+  onclick: content.deleteAllActivities
+});
+chrome.contextMenus.create({
+  title: "Clear Local Storage",
+  parentId: "keats_progress",
+  onclick: clear_storage
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,67 +5,186 @@ var keatsProgress = {
 			callback(response);
 		});
 	},
-	modifyPrgressEntry : function(entry, callback){
-		chrome.runtime.sendMessage({cmd : "processCurrentModuleEntry", payload : entry}, function(response) {
-			console.log(response)
+	directWrite : function(entries, callback){
+		chrome.runtime.sendMessage({cmd : "directWrite", payload : {data : entries}}, function(response) {
+			callback(response)
+		});
+	},
+	modifyPrgressEntry : function(entry, callback, more_coming = false){
+		chrome.runtime.sendMessage({cmd : "processCurrentModuleEntry", payload : {data : entry, more_coming : more_coming}}, function(response) {
+			//console.log(response)
 		});
 		callback(true)
 	},
 	updateProgress : function(percentage){
 		chrome.runtime.sendMessage({cmd : "updateProgress", payload : percentage}, null);
+	},
+	isNavigatedDashboard : function(callback){
+		chrome.runtime.sendMessage({cmd : "isNavigatedDashboard", payload : null}, function(response){
+			callback(response)
+		});
+	}
+}
+//Update visual progress
+var totalNumberOfActivities = 0
+var updateProgress = function(){
+	var percentageCompleted = $('input[type=checkbox]:checked').length/totalNumberOfActivities
+	percentageCompleted = percentageCompleted*100
+	percentageCompleted = percentageCompleted.toFixed(0)
+	keatsProgress.updateProgress(percentageCompleted)
+}
+//Element Parser
+function parseActivityElement(activityElement, status = null){
+	var activityID = $(activityElement).attr('id');
+	var activityName = $(activityElement).attr('activityname');
+	var activityType = $(activityElement).attr('activitytype');
+	if(status == null){
+		var activityChecked = $(activityElement).prop('checked');
+	}else{
+		var activityChecked = status;
+	}
+	var resourceURL = $(activityElement).attr('resourceURL');
+	var entry = {
+		activityID : activityID,
+		activityName : activityName,
+		activityType : activityType,
+		resourceURL : resourceURL,
+		activityStatus : activityChecked
+	}
+	return entry
+}
+var writeToScreen = function(){
+	//Write current module stage to screen
+	keatsProgress.getModuleProgress(function(response){
+		//console.log(response)
+		var moduleProgress = response[Object.keys(response)[0]]
+		if(moduleProgress != undefined){
+			if(moduleProgress.hasOwnProperty("length")){
+				if(moduleProgress.length >= 1){
+					moduleProgress.forEach((activity) => {
+						//console.log(activity)
+						activityID = activity.activityID
+						var activityElement = $(`input[resourceURL="${activity.resourceURL}"]`)
+						//console.log(activityElement.length)
+						if(activityElement != null){
+							activityElement.prop('checked', true);
+						}else{
+							activityElement.prop('checked', false);
+						}
+					});
+				}
+			}
+		}
+		//Update progress badge
+		updateProgress()
+	});
+}
+var injectPage = function(){
+	//Check where we are. This will change operational mode.
+	//console.log("Runing content script...")
+	keatsProgress.isNavigatedDashboard(function(inDashBoard){
+		if(inDashBoard){
+			//User is in dashboard
+		}
+	});
+	//Read saved Progress
+	function handleClick(cb) {
+		//console.log("Clicked, new value = " + cb.checked);
+	}
+	clearInterval(readyStateCheckInterval);
+
+	//Grab all the activity instances
+	var activity = {}
+	totalNumberOfActivities = $(".instancename").length
+	$(".instancename").each(function(activityElementIndex){
+		var activityElement = $(this);
+		var resourceURLElement = $(this).closest('.aalink');
+		if (activityElement.length >= 1){
+			activityName = activityElement[0].innerHTML.substr(0, activityElement[0].innerHTML.indexOf('<span class="accesshide ">'));
+			activityType = activityElement[0].innerHTML.substr(activityElement[0].innerHTML.indexOf('<span class="accesshide ">'));
+			activityType = activityType.replace('<span class="accesshide ">', "")
+			activityType = activityType.replace('</span>', "")
+			resourceURL = resourceURLElement.attr('href');
+		}else{
+			activityName = "undefined"
+			activityType = "undefined"
+		}
+		activityElement.parent().parent().prepend(
+			`<input type="checkbox" id="keats_progress_${activityElementIndex}" activityname="${activityName}" resourceURL="${resourceURL}" activitytype="${activityType.trim()}"  name="keats_progress">`);
+	});
+	writeToScreen()
+	//Listen to Checkboxes
+	$("input:checkbox").change(function(){
+		var entry = parseActivityElement(this)
+		keatsProgress.modifyPrgressEntry(entry,
+			function(changeStatus){
+				//console.log(changeStatus)
+			});
+		//Equate percentage completed
+		updateProgress()
+	});
+}
+var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		injectPage()
+	}
+}, 10);
+
+var send_entries = function(entries, index, callback){
+	keatsProgress.directWrite()
+	if(entries.hasOwnProperty("length")){
+		if(typeof entries[index] === 'undefined'){
+			callback(null)
+			updateProgress()
+		}else{
+			keatsProgress.modifyPrgressEntry(entries[index],
+				function(changedStatus){
+					send_entries(entries, (index+1), callback)
+				}, !(typeof entries[index+1] === 'undefined'));
+		}
 	}
 }
 
-var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		//Read saved Progress
-		var totalNumberOfActivities = 0
-		function handleClick(cb) {
-			console.log("Clicked, new value = " + cb.checked);
-		}
-		clearInterval(readyStateCheckInterval);
-
-		//Grab all the activity instances
-		totalNumberOfActivities = $(".instancename").length
-		$(".instancename").each(function(activityElementIndex){
-			var activityElement = $(this);
-			activityElement.parent().parent().prepend(
-				`<input type="checkbox" id="keats_progress_${activityElementIndex}" name="keats_progress">`);
+var keatsPage = {
+	activities : null,
+	setAllActivitiesCompleted : function(callback){
+		var entries = []
+		var instanceActivities = document.getElementsByName("keats_progress");
+		instanceActivities.forEach(function(activityElement){
+			var entry = parseActivityElement(activityElement, true)
+			entry.activityStatus = true
+			entries.push(entry)
+		});
+		keatsProgress.directWrite(entries, function(err){
+			writeToScreen()
+			//console.log(err)
 		});
 
-		//Write current module stage to screen
-		keatsProgress.getModuleProgress(function(response){
-			var moduleProgress = response[Object.keys(response)[0]]
-			moduleProgress.forEach((activity) => {
-				var activityElement = $(`#${activity}`)
-				if(activityElement != null){
-					activityElement.prop('checked', true);
-				}
-			});
-			//Update progress badge
-			updateProgress()
-		});
-		//Update visual progress
-		var updateProgress = function(){
-			var percentageCompleted = $('input[type=checkbox]:checked').length/totalNumberOfActivities
-			percentageCompleted = percentageCompleted*100
-			percentageCompleted = percentageCompleted.toFixed(0)
-			keatsProgress.updateProgress(percentageCompleted)
-		}
-		//Listen to Checkboxes
-		$("input:checkbox").change(function(){
-			var activityID = $(this).attr('id');
-			var activityChecked = $(this).prop('checked')
-			var entry = {
-				activityID : activityID,
-				activityStatus : activityChecked
-			}
-			keatsProgress.modifyPrgressEntry(entry,
-				function(changeStatus){
-					console.log(changeStatus)
-				});
-			//Equate percentage completed
-			updateProgress()
+	},
+	deleteAllActivities : function(callback){
+		keatsProgress.directWrite([], function(err){
+			callback()
+			//console.log(err)
 		});
 	}
-}, 10);
+}
+
+//Listeners
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	if(request.cmd=="selectAllModules"){
+		keatsPage.setAllActivitiesCompleted(function(err){
+			//console.log(err)
+		});
+	}
+	if(request.cmd=="deleteAllActivities"){
+		keatsPage.deleteAllActivities(function(err){
+			var checkedElements = $('input[type=checkbox]:checked')
+			//console.log(checkedElements)
+			var checkedElementsIndex = 0;
+			for (checkedElementsIndex = 0; checkedElementsIndex < checkedElements.length; checkedElementsIndex++) {
+				$(checkedElements[checkedElementsIndex]).prop('checked', false);
+			}
+			//console.log(err)
+		});
+	}
+});
